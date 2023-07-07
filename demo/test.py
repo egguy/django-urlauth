09d04ce27090f893964dccb9368a8f6f3c0467f4
egguy <etienne.guilluy@gmail.com>
2023-07-07 17:33:14 +1000

    Updated to black + isort standard the last files

--- demo/test.py
+++ demo/test.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-from django.core.management import call_command
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 from django.conf import settings
 from django.contrib.auth import get_user_model
-
+from django.core.management import call_command
 
 from urlauth.models import AuthKey
-
 
 User = get_user_model()
 
 
 def reproduce():
     try:
-        os.unlink(settings.DATABASES['default']['NAME'])
+        os.unlink(settings.DATABASES["default"]["NAME"])
     except OSError:
         pass
-    call_command('syncdb', interactive=False)
-    call_command('flush', interactive=False)
+    call_command("syncdb", interactive=False)
+    call_command("flush", interactive=False)
 
-    print('Creating users')
+    print("Creating users")
     for x in range(100):
-        username = 'test%d' % x
-        User.objects.create_user(username, '%s@gmail.com' % username, username)
+        username = "test%d" % x
+        User.objects.create_user(username, "%s@gmail.com" % username, username)
 
     users = User.objects.all()
 
-    print('Creating auth keys')
+    print("Creating auth keys")
     for user in users:
-        obj = AuthKey.objects.wrap_url('/', uid=user.pk)
+        obj = AuthKey.objects.wrap_url("/", uid=user.pk)
 
-    print('Key count: %d' % AuthKey.objects.count())
+    print("Key count: %d" % AuthKey.objects.count())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     reproduce()
--- setup.py
+++ setup.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='django-urlauth-egguy',
-    version='0.2.2',
-    description='Django application for user authentication with key in hypertext link',
-    url='https://github.com/egguy/django-urlauth/',
-    author='Grigoriy Petukhov',
-    author_email='lorien@lorien.name',
-
-    packages=find_packages(exclude=['demo']),
+    name="django-urlauth-egguy",
+    version="0.2.2",
+    description="Django application for user authentication with key in hypertext link",
+    url="https://github.com/egguy/django-urlauth/",
+    author="Grigoriy Petukhov",
+    author_email="lorien@lorien.name",
+    packages=find_packages(exclude=["demo"]),
     include_package_data=True,
     zip_safe=False,
-
     license="BSD",
     keywords="django application authentication authorization",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Utilities'
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
     ],
     install_requires=[
         "django>=1.11",
-    ]
+    ],
 )
